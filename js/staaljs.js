7c22eede5daa59966df4ba7fc3858e859060cbc3
StikkelMaal <msan37878@edu.ucl.dk>
2022-05-19 14:58:37 +0200

    nu med java

---
+++ js/staaljs.js
@@ -0,0 +1,25 @@
+function returnText(){
+    let creationDate = document.getElementById("cdate").value;
+    alert(creationDate)
+}
+function returnText(){
+    let projectClient = document.getElementById("client").value;
+}
+function returnText(){
+    let mailForSubscribtion = document.getElementById("ymail").value;
+}
+function returnText(){
+    let yourName= document.getElementById("yname").value;
+}
+function returnText(){
+    let monthlyCost= document.getElementById("cost").value;
+}
+function returnText(){
+    let supportMail = document.getElementById("smail").value;
+}
+function returnText(){
+    let functionality = document.getElementById("funct").value;
+}
+function returnText(){
+    let subscriptionName = document.getElementById("sname").value;
+}
