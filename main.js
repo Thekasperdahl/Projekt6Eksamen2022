6420011dddb3786a11e95576a1b10cc583ed76b1
Thekasperdahl <95621877+Thekasperdahl@users.noreply.github.com>
2022-05-23 09:01:12 +0200

    insert

---
+++ main.js
@@ -0,0 +1,37 @@
+// Mathias javascript
+function myFunction() {
+  let checkBox = document.getElementById("myCheck");
+  let text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function enFunction() {
+let knapper = ["hjem", "kalender", "nyheder","vitig_info",];
+let text ="";
+for (let i = 0; i < knapper.length; i++) {
+  text += knapper[i]
+  if(i==3){
+    console.log(text);
+  }
+  else{
+    console.log(i)
+  }
+}
+}
+
+//Lucas javascript
+
+let farver = ["white", "purple", "black","lightblue",];
+let text ="";
+for (let i = 0; i < farver.length; i++) {
+  text += farver [i]
+  if(i==2){
+    console.log(text);
+  }
+  else{
+    console.log(i)
+  }
+}
